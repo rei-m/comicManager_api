733a43e8552c70ca11863a9c5f108e2034100ca2
matsushitar <star-oceans@abelia.ocn.ne.jp>
2014-08-03 20:10:55 +0900

    split business logic

--- app.js
+++ app.js
@@ -5,13 +5,19 @@
 // モジュール読み込み
 module.paths.push('./my_modules/node_modules');
 var restify = require('restify'),
-    bl = require('appBL'),
-    logger = require('logger');
+    logger = require('cmn/logger'),
+    mongoose = require('cmn/cmnDb'),
+    Comic = require('model/comic').getModel(mongoose),
+    SalesInfo = require('model/salesInfo').getModel(mongoose);
+
+// レスポンスロジック処理作成
+var respondMasterInfo = require('comicMaster/respondInfo')(Comic),
+    respondSalesInfo = require('comicSales/respondInfo')(SalesInfo);
 
 // サーバーを作成しルーティングを設定
 var server = restify.createServer();
-server.get('/comic/master', bl.respondMasterInfo);
-server.get('/comic/sales', bl.respondSalesInfo);
+server.get('/comic/master', respondMasterInfo);
+server.get('/comic/sales', respondSalesInfo);
 
 // listen開始
 server.listen(8080, function() {
